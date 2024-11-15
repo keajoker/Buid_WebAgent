ac31569604329b93b6d0ec74f4db670e4e5ccee7
keajoker <119678212+keajoker@users.noreply.github.com>
2024-11-15 16:22:04 +0400

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -61,8 +61,9 @@ const sendMessage = async () => {
     chatMessages.appendChild(userMessageElement);
     chatInput.value = '';
 
+
     // Send user message to server and get response
-    const response = await fetch('http://127.0.0.1:8000', {
+    const response = await fetch('https://f7w3h7d5-8000.inc1.devtunnels.ms/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
